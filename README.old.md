6bae79b2e2fe53a57437671da4d959f651845999
nicoasp <nicolas.amaya@gmail.com>
2017-05-17 17:47:48 +0200

    initial setup and state structure

---
+++ README.old.md
@@ -0,0 +1,35 @@
+# assignment_star_wars_spa
+May the SPA be with you...
+
+state = {
+	People: {
+		data: [],
+		isFetching: false,
+		error. null
+	},
+	Films: {
+		data: [],
+		isFetching: false,
+		error. null
+	},
+	Starships: {
+		data: [],
+		isFetching: false,
+		error. null
+	},
+	Vehicles: {
+		data: [],
+		isFetching: false,
+		error. null
+	},
+	Species: {
+		data: [],
+		isFetching: false,
+		error. null
+	},
+	Planets: {
+		data: [],
+		isFetching: false,
+		error. null
+	},
+}
---
+++ src/components/App.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch,
+} from 'react-router-dom'
+
+import ScrollToTop from './ScrollToTop'
+
+const NavLinks = () => (
+  <div className="NavLinks">
+    <NavLink activeClassName="active" exact to="/">
+      Home
+    </NavLink>{' '}
+    <NavLink activeClassName="active" exact to="/films">
+      Films
+    </NavLink>{' '}
+    <NavLink activeClassName="active" to="/people">
+      People
+    </NavLink>{' '}
+    <NavLink activeClassName="active" exact to="/planets">
+      Planets
+    </NavLink>{' '}
+    <NavLink activeClassName="active" exact to="/species">
+      Species
+    </NavLink>{' '}
+    <NavLink activeClassName="active" to="/starships">
+      Starships
+    </NavLink>
+    <NavLink activeClassName="active" to="/vehicles">
+      Vehicles
+    </NavLink>  
+  </div>
+)
+
+const App = () => (
+  <Router>
+    <ScrollToTop>
+      <NavLinks />
+
+      <Switch>
+        <Route exact path="/" render={() => <h1>Home</h1>} />
+        <Route render={() => <h1>Page not found</h1>} />
+      </Switch>
+
+      <NavLinks />
+    </ScrollToTop>
+  </Router>
+)
+
+export default App
